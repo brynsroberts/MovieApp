32814d824dacc2acfc17ed1954f2162b032ea1f1
Bryan Roberts <bryan.roberts.cs@gmail.com>
2020-04-28 15:36:32 -0700

    refactor autocomplete functionality

--- index.js
+++ index.js
@@ -1,36 +1,82 @@
 var myKey = config.API_KEY;
 
-const fetchData = async (searchTerm) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: myKey,
-      s: searchTerm,
-    },
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
-const input = document.querySelector("input");
-
-
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-  for (let movie of movies) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-    <img src="${movie.Poster}"/>
-    <h1>${movie.Title}</h1>
+createAutoComplete({
+	root: document.querySelector('.autocomplete'),
+	renderOption(movie) {
+		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+		return `
+    <img src="${imgSrc}"/>
+    ${movie.Title}
     `;
+	},
+	onOptionSelect(movie) {
+		onMovieSelect(movie);
+	},
+	inputValue(movie) {
+		return movie.Title;
+	},
+	async fetchData(searchTerm) {
+		const response = await axios.get('http://www.omdbapi.com/', {
+			params: {
+				apikey: myKey,
+				s: searchTerm
+			}
+		});
 
-    document.querySelector('#target').appendChild(div);
-  }
+		if (response.data.Error) {
+			return [];
+		}
+
+		return response.data.Search;
+	}
+});
+
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: myKey,
+			i: movie.imdbID
+		}
+	});
+
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
 
-input.addEventListener("input", debounce(onInput, 500));
+const movieTemplate = (movieDetail) => {
+	return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content>
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
+};
